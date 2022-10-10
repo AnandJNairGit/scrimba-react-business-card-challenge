b881f51af9668d1f0552ead52dbc9bf8292ae6b7
AnandJNairGit <anandjnairit@gmail.com>
2022-10-10 14:29:09 +0530

    add profile details section

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+function PersonDetails(){
+    return(<div>
+        <h1 className="name">Anand J Nair</h1>
+        <h5 className="occupation">Frontend Developer</h5>
+        <a className="website">anand.website</a>
+
+    </div>);
+}
+
 export default function ProfileDetail() {
   return (
     <>
-      <section className="profile-detail"></section>
+      <section className="profile-detail">
+        <PersonDetails />
+      </section>
     </>
   );
 }
