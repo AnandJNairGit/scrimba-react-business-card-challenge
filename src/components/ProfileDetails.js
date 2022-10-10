fb54a85973fe4b2cd2cfa8769b6aa78c2fd379a2
AnandJNairGit <anandjnairit@gmail.com>
2022-10-10 15:47:15 +0530

    addbutton component

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -1,12 +1,29 @@
 import React from "react";
+import email from "../assets/email.png";
+import twitter from "../assets/Twitter.png";
+import linked from "../assets/linked.png";
 
-function PersonDetails(){
-    return(<div>
-        <h1 className="name">Anand J Nair</h1>
-        <h5 className="occupation">Frontend Developer</h5>
-        <a className="website">anand.website</a>
+function PersonDetails() {
+  return (
+    <div className="person-detail-container">
+      <h1 className="name">Anand J Nair</h1>
+      <h5 className="occupation">Frontend Developer</h5>
+      <a className="website">anand.website</a>
+    </div>
+  );
+}
 
-    </div>);
+function ConnectButtons() {
+  return (
+    <div className="buttons">
+      <button className="email-btn">
+        <img className="button-icon" src={email} /> <p>Email</p>
+      </button>
+      <button className="linkedIn-btn">
+        <img className="button-icon" src={linked} /> <p>LinkedIn</p>
+      </button>
+    </div>
+  );
 }
 
 export default function ProfileDetail() {
@@ -14,6 +31,7 @@ export default function ProfileDetail() {
     <>
       <section className="profile-detail">
         <PersonDetails />
+        <ConnectButtons />
       </section>
     </>
   );
